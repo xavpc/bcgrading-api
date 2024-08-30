a8e6d7a6df795afbed9a50741aa189a32342d306
xav <xav1234g@gmail.com>
2024-08-30 13:57:36 +0800

    removed unsused api routes sa registrar

--- -registrars/registrars.controller.js
+++ -registrars/registrars.controller.js
@@ -14,23 +14,6 @@ router.get('/semesters' ,authorize([Role.Admin, Role.Registrar]), getAllSemester
 router.get('/subjects', authorize([Role.Admin, Role.Registrar]), getAllSubject);
 
 
-router.get('/year/:year', authorize([Role.Admin, Role.Registrar]), getClassesByYear);
-router.get('/semester/:semester', authorize([Role.Admin, Role.Registrar]),getClassesBySemester);
-router.get('/subject/:subjectcode',authorize([Role.Admin, Role.Registrar]), getClassesBySubject);
-router.get('/year/:year/semester/:semester',authorize([Role.Admin, Role.Registrar]), getClassesByYearAndSemester);
-
-router.get('/semester/:semester/year/:year',authorize([Role.Admin, Role.Registrar]), getClassesByYearAndSemester);
-
-router.get('/year/:year/subject/:subjectcode',authorize([Role.Admin, Role.Registrar]), getClassesByYearAndSubject);
-
-router.get('/subject/:subjectcode/year/:year',authorize([Role.Admin, Role.Registrar]), getClassesByYearAndSubject);
-
-router.get('/semester/:semester/subject/:subjectcode',authorize([Role.Admin, Role.Registrar]), getClassesBySemesterAndSubject);
-
-router.get('/subject/:subjectcode/semester/:semester',authorize([Role.Admin, Role.Registrar]), getClassesBySemesterAndSubject);
-
-router.get('/year/:year/semester/:semester/subject/:subjectcode',authorize([Role.Admin, Role.Registrar]), getClassesByYearAndSemesterAndSubject);
-
 router.post('/addclass', authorize(Role.Registrar), newclassSchema,addnewclass);
 router.post('/addsubject', authorize(Role.Registrar), newsubjectSchema,addSubject);
 
@@ -60,46 +43,6 @@ function getAllSubject(req, res, next) {
 }
 
 
-
-function getClassesByYear(req, res, next) {
-    Service.getClassesByYear(req.params.year)
-        .then(classsesbyyear => res.json(classsesbyyear))
-        .catch(next)
-}
-
-function getClassesBySemester(req, res, next) {
-    Service.getClassesBySemester(req.params.semester)
-        .then(classsesbysemester => res.json(classsesbysemester))
-        .catch(next)
-}
-
-function getClassesBySubject(req, res, next) {
-    Service.getClassesBySubject(req.params.subjectcode)
-        .then(classesbysubjectcodes => res.json(classesbysubjectcodes))
-        .catch(next)
-}
-
-function getClassesByYearAndSemester(req, res, next) {
-    Service.getClassesByYearAndSemester(req.params.year, req.params.semester)
-        .then(yearandsemester => res.json(yearandsemester))
-        .catch(next)
-}
-function getClassesByYearAndSubject(req, res, next) {
-    Service.getClassesByYearAndSubject(req.params.year, req.params.subjectcode)
-        .then(yearandsubjectr => res.json(yearandsubjectr))
-        .catch(next)
-}
-function getClassesBySemesterAndSubject(req, res, next) {
-    Service.getClassesBySemesterAndSubject(req.params.subjectcode, req.params.semester)
-        .then(semesterandsubject => res.json(semesterandsubject)) 
-        .catch(next)
-}
-
-function getClassesByYearAndSemesterAndSubject(req, res, next) {
-    Service.getClassesByYearAndSemesterAndSubject(req.params.year, req.params.semester, req.params.subjectcode)
-        .then(yearandsemesterandsubject => res.json(yearandsemesterandsubject))
-        .catch(next)
-}
 
 
 function addnewclass(req, res, next) {
