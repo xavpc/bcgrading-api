eb9236417611a9f74bb74eb4aab371bec0a99c58
xav <xav1234g@gmail.com>
2024-10-12 22:11:56 +0800

    cors

--- server.js
+++ server.js
@@ -11,7 +11,29 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+// app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+
+const allowedOrigins = [
+    "https://bcgrading.netlify.app/", // Production frontend
+    "http://localhost:5173", // Development frontend
+  ];
+  
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps, curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg =
+            "The CORS policy for this site does not allow access from the specified Origin.";
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+      credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    })
+  );
 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
